bb138db0db97ead5d6a27f37acfe223e1ca880c2
ndavis <nicholas.davis@metecs.com>
2022-02-07 09:54:08 -0600

    Project Cleanup

--- vendor/suits_frontend/models/SimulationFailure.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose')
-var Schema = mongoose.Schema
-
-var SimulationFailure = new Schema({
-	started_at: { type: Date, required: true },
-	fan_error: {type: Boolean, required: true}
-})
-module.exports = mongoose.model('SimulationFailure', SimulationFailure)
